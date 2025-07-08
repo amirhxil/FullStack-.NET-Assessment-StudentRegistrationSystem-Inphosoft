aad7b3f17a740d58390e5c9b70371c339037903e
Muhammad Amir Hail <amir.ryugasaki@gmail.com>
2025-07-08 12:02:10 +0800

    Add project files.

---
+++ Data/StudentDbContext.cs
@@ -0,0 +1,13 @@
+﻿namespace StudentRegistrationSystem.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using StudentRegistrationSystem.Models;
+
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options) { }
+
+        public DbSet<Student> Students { get; set; }
+    }
+
+}
---
+++ Models/Student.cs
@@ -0,0 +1,18 @@
+﻿
+namespace StudentRegistrationSystem.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime RegistrationDate { get; set; } = DateTime.Now;
+    }
+
+}
